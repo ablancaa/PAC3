933f76b4dcc374b6133ead0c1411ee292f5e324c
ablancaa <88708431+ablancaa@users.noreply.github.com>
2022-04-13 19:03:53 +0200

    Cambios en SearchBar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,22 +1,51 @@
 <template>
   <div class="search">
     <div>
-    <input type="text" id="consulta" v-model="consulta" placeholder="Search for a recipe" /><button v-if="consulta != ''" @click="clearSearch">Clear Search</button>
+    <form @submit.prevent="busqueda"> 
+       <input type="text" id="consulta" v-model.trim="consulta1" placeholder="Search for a recipe" />
+       <button v-if="consulta1 != ''" @click="clearSearch">Clear Search</button>
+    </form>
     </div>
+    
     <button @click="showForm">Add a new recipe</button>
   </div>
 </template>
 <script>
-import { defineComponent } from "vue";
+
+import { defineComponent, inject, ref } from "vue";
 //import RecipeForm from "@/components/RecipeForm.vue";
 export default defineComponent({
   name: "SearchBar",
    components: {
    //RecipeForm,
   },
+  setup(){
+    const receta = inject('recipe');
+    const consulta1 = ref('');
+    const busqueda = () => {
+      if(consulta1.value === ''){
+        console.log("está vacio");
+       
+        return
+      }
+     
+     console.log(consulta1.value);
+      
+      const recipe = {
+        consulta: consulta1.value,
+      }
+
+      receta.value.push(recipe);
+      consulta1.value='';
+      console.log("Objeto creado de busqueda: "+recipe.consulta);
+    };
+    console.log(receta.value);
+    return {busqueda, consulta1}
+  },
  data() {
     return {
       consulta: '',
+      
     }
   },
   methods: {
@@ -30,13 +59,16 @@ export default defineComponent({
   /* Aquest mètode s'encarregarà de buidar l'element input del camp de cerca.
   S’haurà d’executar quan es faci clic al botó “Clear Search”. */
   clearSearch(){
-       this.consulta = document.getElementById("consulta").value = "";
+       this.consulta = document.getElementById("consulta").value="";
+       //this.consulta1 = '';
        console.log("Función clearSearch(){} Campo reseteado");
   },
    /*Aquest mètode s'executarà cada vegada que es modifiqui l'element
   input del camp de cerca (cada vegada que es teclegi una lletra). Emetrà un esdeveniment
   'search' amb el contingut del camp de cerca */
   search(newVal){
+    this.page = 0;
+    this.
     console.log(newVal);
   },
  }
