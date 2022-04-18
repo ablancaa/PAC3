300f32c0942ce78bdacd7b28ffda4ba75c38232e
ablancaa <88708431+ablancaa@users.noreply.github.com>
2022-04-18 11:24:19 +0200

    Ingrediente para prueba

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,7 @@ export default defineComponent({
         time: "30m",
         difficulty: "Easy",
         title: "Spaghetti",
-        ingredients: ["noodles", "tomato sauce", "cheese"],
+        ingredients: ["noodles", "tomato sauce", "cheese", "oregano"],
         directions: ["boil noodles", "cook noodles", "eat noodles"],
         imageUrl:
           "https://imagesvc.meredithcorp.io/v3/mm/image?q=60&c=sc&poi=face&w=2000&h=1000&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F21%2F2018%2F02%2F14%2Frecetas-4115-spaghetti-boloesa-facil-2000.jpg",
